8dffa05c993541bc7e2848fc5ca76f4fd4def939
Muhammad Afuzarahman <theafuza@gmail.com>
2023-09-05 21:09:46 +0700

    Styled mobile navbar

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import logoImg from "../assets/logo.png";
 
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <header>
-      <img src={logoImg} alt="Logo" width={200} />
-      <nav>
+      <img id="header-logo" src={logoImg} alt="Logo" width={200} />
+      <nav id="header-nav-mobile">
+        <ul className={isNavOpen ? "opened" : ""}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/menu">Menu</Link>
+          </li>
+          <li>
+            <Link to="/reservation">Reservation</Link>
+          </li>
+          <li>
+            <Link to="/order">Order Online</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <div id="header-hamburger" onClick={() => setIsNavOpen((prev) => !prev)}>
+        {isNavOpen ? (
+          <i class="fa-solid fa-xmark"></i>
+        ) : (
+          <i class="fa-solid fa-bars"></i>
+        )}
+      </div>
+      <nav id="header-nav-horizontal">
         <ul>
           <li>
             <Link to="/">Home</Link>
