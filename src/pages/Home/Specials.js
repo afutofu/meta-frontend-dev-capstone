ee2f800550968c483605b7b62a21162dc1d19f22
Muhammad Afuzarahman <theafuza@gmail.com>
2023-09-06 16:31:45 +0700

    Add specials section

--- src/pages/Home/Specials.js
+++ src/pages/Home/Specials.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import greekSaladImg from "../../assets/greek-salad.jpg";
 
 const SPECIAL_MENU_ITEMS = [
   {
     name: "Greek Salad",
+    image: greekSaladImg,
     price: 20,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
   },
   {
     name: "Greek Salad",
+    image: greekSaladImg,
     price: 20,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
   },
   {
     name: "Greek Salad",
+    image: greekSaladImg,
     price: 20,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
@@ -24,30 +28,40 @@ const SPECIAL_MENU_ITEMS = [
 
 const Specials = () => {
   return (
-    <div className="container">
-      <div>
+    <section id="specials" className="container">
+      <div id="specials-header">
         <h2>Specials</h2>
         <button>Order Online</button>
       </div>
-      <div>
-        {SPECIAL_MENU_ITEMS.map(({ name, price, description }) => (
-          <SpecialsCard name={name} price={price} description={description} />
-        ))}
+      <div id="specials-list">
+        {SPECIAL_MENU_ITEMS.map(
+          ({ name, image, price, description }, index) => (
+            <SpecialsCard
+              name={name}
+              image={image}
+              price={price}
+              description={description}
+              key={index}
+            />
+          )
+        )}
       </div>
-    </div>
+    </section>
   );
 };
 
-const SpecialsCard = ({ name, price, description }) => {
+const SpecialsCard = ({ name, image, price, description }) => {
   return (
-    <div>
-      <img id="specials-card-image" alt={name} />
-      <div>
-        <h3>{name}</h3>
-        <span>{price}</span>
+    <div className="specials-card">
+      <img src={image} className="specials-card-image" alt={name} />
+      <div className="specials-card-texts">
+        <div className="specials-card-header">
+          <h3>{name}</h3>
+          <span>{price}</span>
+        </div>
+        <p>{description}</p>
+        <Link to="/order">Order a delivery</Link>
       </div>
-      <p>{description}</p>
-      <Link to="/order">Order a delivery</Link>
     </div>
   );
 };
